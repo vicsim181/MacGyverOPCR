1ae194dcd0b799e3603d9fe3c671cc58a01d5306
vicsim181 <victor.simon@outlook.com>
2020-07-07 13:19:59 +0200

    Ajouts mouvs, collisions, objets, victoire ou non

--- game.py
+++ game.py
@@ -1,16 +1,15 @@
-import maze
+"""Module where the main function is host"""
 import random 
 
+import maze
 
-inventory=[]
-needed=('plastic tube', 'ether', 'needle')
+inventory = []
+needed = ('plastic tube', 'ether', 'needle')
 
-def movements():
-    pass
-
-
-def main():
+"""Function launching the game and calling the maze"""
+def main(): 
     maze
-    number = random.randint(0,10)
+    number = random.randint(0, 10)
     print(number)
+
 main()
--- maze.py
+++ maze.py
@@ -1,21 +1,22 @@
 import random
 
-tools={"plastic tube":"1", "ether":"2", "needle":"3"}
-global last_pos
-last_pos = 0  
+tools={1: "plastic tube",2: "ether",3: "needle"} 
 
+"""Class allowing to create characters in the game"""
 class Character():
     def __init__(self, pos, inventory):
         self.pos = pos 
         self.inventory = inventory
 
+
+"""Class allowing to create different exceptions when moving MacGyver in the maze"""
+class Exceptions():
+    def __init__(self):
+        pass
+
 macgyver = Character(0,[])
 
-class Error_Mouvement():
-    def __init__(self):
-
-
-#Fonction qui génère et modifie le labyrinthe à partir du fichier CSV
+"""Fonction qui génère et modifie le labyrinthe à partir du fichier CSV"""
 def maze_creation():
     with open("Maze.csv", encoding="utf8") as fichier:
         data= fichier.read()
@@ -47,116 +48,104 @@ def maze_creation():
         liste4=[int(number) for number in liste3]
     return liste4
 
-# Function finding the free spots in the maze, to later place the items
+"""Function finding the free spots in the maze, to later place the items"""
 def get_free_locations(maze):    
     return [i for i in range(0,len(maze)) if maze[i] == 0]
 
-# Function allowing random places from the free spots to the items    
+"""Function allowing random places from the free spots to the items""" 
 def place_items(maze, places):
     for index, item in enumerate(tools):
         item_pos=random.choice(places)
         places.remove(item_pos)
         maze[item_pos]= index + 2
 
-symbols={0: "  ", 1: "██", 2: "Ꝣ ", 3: "ꞵ ", 4: "ꜫ ", 5: "M ", 6: "G "}
+symbols={0: "  ", 1: "██", 2: "Ꝣ ", 3: "ꞵ ", 4: "ꜫ ", 6: "G ", 42602: "Ꙫ "}
 
-# Function that draws the maze on the terminal for the player 
+"""Function that draws the maze on the terminal for the player"""
 def draw_maze(maze, symbols):
     for index, tile in enumerate(maze):
         print(symbols[tile], end="\n" if index % 15 == 14 else "")
 
-# Function asking what action the player wants to realise. It then call the appropriate function, movement, or exiting.
+"""Function asking what action the player wants to realise. 
+It then call the appropriate function, movement, or exiting.
+"""
 def action(last_pos):
-    action = input(print("What action do you want to make?\n", "To quit the game, press Q.\n", "To move MacGyver, press 'R' to move right, 'L' to move left, 'U' to move upward, 'D' to move downward."))
-    print(action)
+    action = input(print("What action do you want to make?\n", "To quit the game, press Q.\n",
+    "To move MacGyver, press 'R' to move right, 'L' to move left, 'U' to move upward, 'D' to move downward."))
     if action == "U" or action == "u":
-        movement(maze1, "U", last_pos)
+        movement(maze1, "U", mouv=-15)
         draw_maze(maze1, symbols)
     elif action == "D" or action == "d":
-        movement(maze1, "D", last_pos)
+        movement(maze1, "D", mouv=15)
         draw_maze(maze1, symbols)
     elif action == "L" or action == "l":
-        movement(maze1, "L", last_pos)
+        movement(maze1, "L", mouv=-1)
         draw_maze(maze1, symbols)
     elif action == "R" or action == "r":
-        movement(maze1, "R", last_pos)
+        movement(maze1, "R", mouv=1)
         draw_maze(maze1, symbols)
     elif action == "Q" or action == "q":
         exiting()
-    # if winning(tools) == True:
-    #     print("Well done! MacGyver fough the guardian and won! Exiting the game")
-    #     exit()
     
-# Movement function in case the player asks for it. Depending on the letter entered, MacGyver will move to the right, left, up or down. 
-def movement(maze, direction, last_pos):
-    if direction == "R":
-        mouv = 1
-    elif direction == "L":
-        mouv = -1
-    elif direction == "U":
-        mouv = -15
-    elif direction == "D":
-        mouv = 15
-    ###################################################################################################################################
-    ######################## Voir comment intégrer des erreurs au lieu de plusieurs If 
+"""Movement function in case the player asks for it. Depending on the letter entered, 
+MacGyver will move to the right, left, up or down.
+""" 
+def movement(maze, direction, mouv):
     if maze[macgyver.pos + mouv] == 1:
-        return WallError
+        print("MacGyver can't move through a wall!\n")
     elif maze[macgyver.pos + mouv] == 2:
-        return Item2
-    elif maze[macgyver.pos + mouv] == 3:
-        return Item3
-    elif maze[macgyver.pos + mouv] == 4:
-        return Item4 
-    try:    
-        maze[macgyver.pos] = 0
-        maze[macgyver.pos + mouv] = 5
-        macgyver.pos = macgyver.pos + mouv
-    except WallError:   
-        print("MacGyver can't move ") 
-    except Item2:   
         print("MacGyver collected the Ꝣ object!")
-        macgyver.inventory.append(tools(2))
+        macgyver.inventory.append(tools[1])
         print(macgyver.inventory)
         maze[macgyver.pos] = 0
-        maze[macgyver.pos + mouv] = 5
+        maze[macgyver.pos + mouv] = 42602
         macgyver.pos = macgyver.pos + mouv
-    except Item3:   
+    elif maze[macgyver.pos + mouv] == 3:
         print("MacGyver collected the ꞵ object!")
-        macgyver.inventory.append(tools(3))
+        macgyver.inventory.append(tools[2])
         print(macgyver.inventory)
         maze[macgyver.pos] = 0
-        maze[macgyver.pos + mouv] = 5
+        maze[macgyver.pos + mouv] = 42602
         macgyver.pos = macgyver.pos + mouv
-    except Item4:   
+    elif maze[macgyver.pos + mouv] == 4:
         print("MacGyver collected the ꜫ object!")
-        macgyver.inventory.append(tools(4))
+        macgyver.inventory.append(tools[3])
         print(macgyver.inventory)
         maze[macgyver.pos] = 0
-        maze[macgyver.pos + mouv] = 5
-        macgyver.pos = macgyver.pos + mouv
-
-# Exiting function when the player wants to quit
+        maze[macgyver.pos + mouv] = 42602
+        macgyver.pos = macgyver.pos + mouv 
+    elif maze[macgyver.pos + mouv] == 6:
+        if winning() == True:
+            print("Well done! MacGyver fought the guardian and won! Exiting the game")
+            exit()
+        else:
+            print("MacGyver didn't collect all the necessary items! The guardian wins!")
+            exit()
+    else:
+        maze[macgyver.pos] = 0
+        maze[macgyver.pos + mouv] = 42602
+        macgyver.pos = macgyver.pos + mouv   
+        
+"""Exiting function when the player wants to quit"""
 def exiting():
     print("MacGyver gives up and quit the game!")
     exit()
 
-# Function which determines if we won the game
+"""Function which determines if we won the game"""
 def winning():
     verification = 0
     for element in tools.keys():
-    # if all (keys for keys in tools in macgyver.inventory and macgyver.pos == 239):
-        if element in macgyver.inventory:
+        if tools[element] in macgyver.inventory:
             verification += 1
     if verification == 3:
         return True
-    else:
-        return False
 
   
 maze1 = maze_creation()
 available = get_free_locations(maze1)
 place_items(maze1, available)
 draw_maze(maze1, symbols)  
-while winning() == False:
+loop = 1
+while loop == 1:
     action(macgyver.pos)     
 
